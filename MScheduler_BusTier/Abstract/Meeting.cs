78908e35cf5a5a4abaf0ca4c646e563fce00c09e
enigmaticcam <enigmaticcam@hotmail.com>
2014-07-03 17:47:34 -0700

    Initial Checkin

---
+++ MScheduler_BusTier/Abstract/Meeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MScheduler_BusTier.Abstract {
+    public interface IMeeting {
+
+    }
+
+    public class Meeting {
+    }
+}
---
+++ MScheduler_BusTier/Abstract/Slot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MScheduler_BusTier.Abstract {
+    class Slot {
+    }
+}
