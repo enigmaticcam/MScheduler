d740ed76adfb8624701a172bd8e9d34029593d48
enigmaticcam <enigmaticcam@hotmail.com>
2014-07-05 08:54:01 -0700

    Creating Meeting and Slot abstracts and decorators

--- MScheduler_BusTier/Abstract/Meeting.cs
+++ MScheduler_BusTier/Abstract/Meeting.cs
@@ -6,9 +6,66 @@ using System.Threading.Tasks;
 
 namespace MScheduler_BusTier.Abstract {
     public interface IMeeting {
-
+        public int Id;
+        public string Description;
+        public DateTime Date;
+        public IEnumerable<ISlot> Slots;
     }
 
-    public class Meeting {
+    public abstract class Meeting {
+        private MeetingData _meetingData;
+        protected MeetingData PrivateData {
+            get { return _meetingData; }
+            set { _meetingData = value; }
+        }
+        public MeetingData Data {
+            set { _meetingData = value; }
+        }
+
+        public int Id {
+            get { return _meetingData.Id; }
+        }
+
+        public string Description {
+            get { return _meetingData.Description; }
+        }
+
+        public DateTime Date {
+            get { return _meetingData.Date; }
+        }
+
+        public IEnumerable<ISlot> Slots {
+            get { return _meetingData.Slots.OrderBy(s => s.SortNumber); }
+        }
+
+        public class MeetingData {
+            public int Id;
+            public string Description;
+            public DateTime Date;
+            public List<ISlot> Slots { get; set; }
+        }
+    }
+
+    public abstract class MeetingDecorator : IMeeting {
+        private IMeeting _meeting;
+        public MeetingDecorator(IMeeting meeting) {
+            _meeting = meeting;
+        }
+
+        public virtual int Id {
+            get { return _meeting.Id; }
+        }
+
+        public virtual string Description {
+            get { return _meeting.Description; }
+        }
+
+        public virtual DateTime Date {
+            get { return _meeting.Date; }
+        }
+
+        public virtual IEnumerable<ISlot> Slots {
+            get { return _meeting.Slots; }
+        }
     }
 }
--- MScheduler_BusTier/Abstract/Slot.cs
+++ MScheduler_BusTier/Abstract/Slot.cs
@@ -6,14 +6,70 @@ using System.Threading.Tasks;
 
 namespace MScheduler_BusTier.Abstract {
     public interface ISlotFiller {
-
+        int Id { get; }
+        string Description { get; }
     }
 
     public interface ISlot {
-
+        Slot.SlotData Data { set; }
+        ISlotFiller Filler { get; }
+        string Description { get; }
+        int SortNumber { get; }
     }
 
-    public class Slot {
+    public abstract class Slot : ISlot {
+        private SlotData _slotData;
+        protected SlotData PrivateData {
+            get { return _slotData; }
+            set { _slotData = value; }
+        }
+        public SlotData Data {
+            set { _slotData = value; }
+        }
 
+        public enum SlotType {
+            User = 0
+        }
+
+        public ISlotFiller Filler {
+            get { return _slotData.Filler; }
+        }
+
+        public string Description {
+            get { return _slotData.Description; }
+        }
+
+        public int SortNumber {
+            get { return _slotData.SortNumber; }
+        }
+
+        public class SlotData {
+            public ISlotFiller Filler { get; set; }
+            public string Description { get; set; }
+            public int SortNumber { get; set; }
+        }
+    }
+
+    public abstract class SlotDecorator : ISlot {
+        private ISlot _slot;
+        public SlotDecorator(ISlot slot) {
+            _slot = slot;
+        }
+
+        public virtual Slot.SlotData Data {
+            set { _slot.Data = value; }
+        }
+
+        public virtual ISlotFiller Filler {
+            get { return _slot.Filler; }
+        }
+
+        public virtual string Description {
+            get { return _slot.Description; }
+        }
+
+        public virtual int SortNumber {
+            get { return _slot.SortNumber; }
+        }
     }
 }
