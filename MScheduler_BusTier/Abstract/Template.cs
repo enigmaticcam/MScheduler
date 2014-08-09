4555e201f4fdee768af60387a202f059e19970cf
enigmaticcam <enigmaticcam@hotmail.com>
2014-08-09 15:23:57 -0700

    Added Template object

---
+++ MScheduler_BusTier/Abstract/Template.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MScheduler_BusTier.Abstract;
+
+namespace MScheduler_BusTier.Abstract {
+    public interface ITemplate {
+        Template.TemplateData Data { set; }
+        int Id { get; }
+        string Description { get; }
+        IEnumerable<TemplateSlot> TemplateSlots { get; }
+        List<ISlot> GenerateMeetingSlots();
+    }
+
+    public abstract class Template : ITemplate {
+        private IFactory _factory;
+
+        private TemplateData _templateData;
+        protected TemplateData PrivateData {
+            get { return _templateData; }
+            set { _templateData = value; }
+        }
+        public TemplateData Data {
+            set { _templateData = value; }
+        }
+
+        public Template(IFactory factory) {
+            _factory = factory;
+        }
+
+        public int Id {
+            get { return _templateData.Id; }
+        }
+
+        public string Description {
+            get { return _templateData.Description; }
+        }
+
+        public IEnumerable<TemplateSlot> TemplateSlots {
+            get { return _templateData.TemplateSlots.OrderBy(t => t.SortNumber); }
+        }
+
+        public List<ISlot> GenerateMeetingSlots() {
+            ActionGenerateMeetingSlots action = new ActionGenerateMeetingSlots.Builder()
+                .SetFactory(_factory)
+                .SetTemplateSlots(_templateData.TemplateSlots)
+                .Build();
+            return action.PerformAction();
+        }
+
+        public class ActionGenerateMeetingSlots {
+            private List<TemplateSlot> _templateSlots;
+            private IFactory _factory;
+            private List<ISlot> _slots;
+
+            public List<ISlot> PerformAction() {
+                _slots = new List<ISlot>();
+                LoopThroughTemplateSlotsAndGenerate();
+                return _slots;
+            }
+
+            private void LoopThroughTemplateSlotsAndGenerate() {
+                foreach (TemplateSlot templateSlot in _templateSlots.OrderBy(t => t.SortNumber)) {
+                    _slots.Add(GenerateSlotFromTemplateSlot(templateSlot));
+                }
+            }
+
+            private ISlot GenerateSlotFromTemplateSlot(TemplateSlot templateSlot) {
+                Slot.SlotData slotData = CopyTemplateSlotDataToSlot(templateSlot);
+                ISlot slot = CreateSlotWithSlotData(slotData);
+                return slot;
+            }
+
+            private Slot.SlotData CopyTemplateSlotDataToSlot(TemplateSlot templateSlot) {
+                Slot.SlotData slotData = new Slot.SlotData();
+                slotData.Title = templateSlot.Title;
+                slotData.SortNumber = templateSlot.SortNumber;
+                return slotData;
+            }
+
+            private ISlot CreateSlotWithSlotData(Slot.SlotData slotData) {
+                ISlot slot = _factory.CreateSlot();
+                slot.Data = slotData;
+                return slot;
+            }
+
+            public ActionGenerateMeetingSlots(Builder builder) {
+                _templateSlots = builder.TemplateSlots;
+                _factory = builder.Factory;
+            }
+
+            public class Builder {
+                public List<TemplateSlot> TemplateSlots;
+                public IFactory Factory;
+
+                public Builder SetTemplateSlots(List<TemplateSlot> templateSlots) {
+                    this.TemplateSlots = templateSlots;
+                    return this;
+                }
+
+                public Builder SetFactory(IFactory factory) {
+                    this.Factory = factory;
+                    return this;
+                }
+
+                public ActionGenerateMeetingSlots Build() {
+                    return new ActionGenerateMeetingSlots(this);
+                }
+            }
+        }
+
+        public class TemplateData {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public List<TemplateSlot> TemplateSlots { get; set; }
+        }        
+    }
+
+    public class TemplateSlot {
+        public int Id { get; set; }
+        public Slot.SlotType SlotType { get; set; }
+        public string Title { get; set; }
+        public int SortNumber { get; set; }
+    }
+
+    public class TemplateDecorator : ITemplate {
+        private ITemplate _template;
+        public TemplateDecorator(Template template) {
+            _template = template;
+        }
+
+        public virtual Template.TemplateData Data {
+            set { _template.Data = value; }
+        }
+
+        public virtual int Id {
+            get { return _template.Id; }
+        }
+        
+        public virtual string Description {
+            get { return _template.Description; }
+        }
+
+        public virtual IEnumerable<TemplateSlot> TemplateSlots {
+            get { return _template.TemplateSlots; }
+        }
+
+        public virtual List<ISlot> GenerateMeetingSlots() {
+            return _template.GenerateMeetingSlots();
+        }
+    }
+}
